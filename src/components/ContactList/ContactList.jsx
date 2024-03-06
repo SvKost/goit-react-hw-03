b0407756df22d84a1aa54ad0bfeda6434306588f
SvKost <skost0634610557@gmail.com>
2024-03-06 21:53:33 +0200

    Created components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,12 @@
+import Contact from "../Contact/Contact";
+
+const ContactList = () => {
+  return (
+    <>
+      <ContactList />
+      <Contact />
+    </>
+  );
+};
+
+export default ContactList;
