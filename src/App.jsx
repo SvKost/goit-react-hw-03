887b6654efa4e581349e08a2cafa257e0d3cf8ba
SvKost <skost0634610557@gmail.com>
2024-03-09 18:58:33 +0200

    add, delete contacts, local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-// import { useState } from "react";
+import initialContacts from "./contacts.json";
+import { useEffect, useState } from "react";
+// import { nanoid } from "nanoid";
 
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
@@ -6,12 +8,41 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState(() => {
+    const stringifiedContacts = localStorage.getItem("contacts");
+    if (!stringifiedContacts) return initialContacts;
+
+    const parsedContacts = JSON.parse(stringifiedContacts);
+    return parsedContacts;
+  });
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={search} onSearching={setSearch} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
   );
 }
